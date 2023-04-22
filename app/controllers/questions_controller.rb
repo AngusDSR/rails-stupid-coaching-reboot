316ffa18679a0e47e2f0f9e5a35fbc78c3852eb2
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-04-22 21:02:39 +0100

    Questions works/emojis added/instance variables added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,18 @@
+# Questions controller
 class QuestionsController < ApplicationController
-  def ask
+  def ask; end
 
+  def answer
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+      @emoji = '🧏'
+    elsif @question =~ /i am going to work/i
+      @answer = 'Great!'
+      @emoji = '😀'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+      @emoji = '😠'
+    end
   end
 end
